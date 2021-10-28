153f90829b3fafbc2dc350b93c85524e55ae8b59
UnbroKen <unbroken2650@gmail.com>
2021-10-28 15:21:11 +0900

    default setting

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # smokemapKR
+
+Making map of smoking area in Korea.
+
+###### https://navermaps.github.io/maps.js.ncp/docs/tutorial-2-Getting-Started.html
---
+++ src/main.js
@@ -0,0 +1,6 @@
+var mapOptions = {
+    center: new naver.maps.LatLng(37.4946447265009, 126.95993178216192),
+    zoom: 10
+};
+
+var map = new naver.maps.Map('map', mapOptions);
