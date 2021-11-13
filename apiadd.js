8a8dc537e4872313e444e36102141b1fb4bbba01
SamSim <jun78111209@gmail.com>
2021-11-13 21:32:03 +0900

    수정

--- apiadd.js
+++ apiadd.js
@@ -1,8 +1,13 @@
-fetch("classified.json")
+/*fetch("classified.json")
     .then((response) => response.json())
     .then((json) => {
         const kakaoApiUrl =
             "//dapi.kakao.com/v2/maps/sdk.js?appkey=" + json.keys.KAKAO_API_KEY;
         document.querySelector("script#kakao").src = kakaoApiUrl;
         console.log(kakaoApiUrl);
-    });
+    });*/
+const kakaoApifront="//dapi.kakao.com/v2/maps/sdk.js?appkey="
+fetch('classified.json')
+.then(response=>{
+    return response.json();
+}).then(apikey=>console.log(apikey));
